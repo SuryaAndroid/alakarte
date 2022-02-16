109fbfd706132eb898dd43d1183e9e8460f89c2a
SuryaAndroid <magnetcreativespub@gmail.com>
2022-02-16 15:21:00 +0530

    hotels 16/02

--- lib/hotels_screen.dart
+++ lib/hotels_screen.dart
@@ -38,144 +38,190 @@ class _HoteslsState extends State<Hotesls> {
       currentMapPosition = LatLng(11.01685, 76.95583);
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:CustomScrollView(
-        slivers: <Widget>[
+        body: NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return <Widget>[
           SliverAppBar(
-            snap: false,
+            expandedHeight: 300.0,
+            //forceElevated: innerBoxIsScrolled,
+            //floating: true,
             pinned: false,
-            floating: false,
-            flexibleSpace: FlexibleSpaceBar(//Text
-                background: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 300,
-                  child: GoogleMap(
-                    zoomGesturesEnabled: true,
-                    myLocationButtonEnabled: true,
-                    tiltGesturesEnabled: true,
-                    scrollGesturesEnabled: true,
-                    rotateGesturesEnabled: true,
-                    zoomControlsEnabled: true,
-                    mapType: MapType.normal,
-                    initialCameraPosition: CameraPosition(
-                      target: currentMapPosition,
-                      zoom: 12,
-                    ),
-                    /*onMapCreated: (GoogleMapController controller) {
-                      _controller = controller;
-                    },*/
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                height: 200,
+                width: double.infinity,
+                child: GoogleMap(
+                  zoomGesturesEnabled: true,
+                  myLocationButtonEnabled: true,
+                  tiltGesturesEnabled: true,
+                  scrollGesturesEnabled: true,
+                  rotateGesturesEnabled: true,
+                  zoomControlsEnabled: true,
+                  mapType: MapType.normal,
+                  initialCameraPosition: CameraPosition(
+                    target: currentMapPosition,
+                    zoom: 12,
                   ),
+                  /*onMapCreated: (GoogleMapController controller) {
+                          _controller = controller;
+                        },*/
                 ),
-            ), //FlexibleSpaceBar
-            expandedHeight: 300,
-          ), //SliverAppBar
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-                  (context, index) => Container(
-                      height:MediaQuery.of(context).size.height*0.2,
-                      width: MediaQuery.of(context).size.width,
-                      child:GestureDetector(
-                        onTap: (){
-                          Navigator.push(
-                              context, MaterialPageRoute(builder: (context) => HomeActivity()));
-                        },
-                        child:Card(
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Container(
-                                    width:100,
-                                    decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                          image: new AssetImage('assets/images/idly.jpg'),fit: BoxFit.cover),
+              ),
+            ),
+          ),
+        ];
+      },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: white, borderRadius: BorderRadius.circular(6)),
+              padding: EdgeInsets.only(
+                left: 6,
+              ),
+              width: double.infinity,
+              alignment: Alignment.center,
+              height: 40,
+              margin: EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(5),
+                    hintText: "Search",
+                    border: InputBorder.none,
+                    suffixIcon: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.search),
+                    )),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 20),
+              alignment: Alignment.centerLeft,
+              width: double.infinity,
+              child: Text(
+                'Hotel list',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.75,
+              width: MediaQuery.of(context).size.width,
+              child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context , index){
+                    return Container(
+                        height: MediaQuery.of(context).size.height * 0.2,
+                        width: MediaQuery.of(context).size.width,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomeActivity()));
+                          },
+                          child: Card(
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: new AssetImage('assets/images/idly.jpg'),
+                                            fit: BoxFit.cover),
+                                      ),
                                     ),
-                                  ),
-                                  Container(
-                                    width: MediaQuery.of(context).size.width-110,
-                                    padding: EdgeInsets.symmetric(horizontal: 6,vertical: 6),
-                                    child: Column(
-                                      // mainAxisAlignment: MainAxisAlignment.center,
-                                      //  crossAxisAlignment: CrossAxisAlignment.center,
-                                      children: [
-                                        Container(
-                                            child:Row(
-                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                              children: [
-                                                Container(
-                                                  child: Text('Hotel Annapoorna pure veg'
-                                                      ,style: TextStyle(fontSize: 16)),
-                                                ),
-                                                Container(
-                                                  padding: EdgeInsets.all(3),
-                                                  decoration: BoxDecoration(
-                                                    borderRadius: BorderRadius.circular(4),
-                                                    border: Border.all(),
+                                    Container(
+                                      width: MediaQuery.of(context).size.width - 110,
+                                      padding:
+                                      EdgeInsets.symmetric(horizontal: 6, vertical: 6),
+                                      child: Column(
+                                        // mainAxisAlignment: MainAxisAlignment.center,
+                                        //  crossAxisAlignment: CrossAxisAlignment.center,
+                                        children: [
+                                          Container(
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                                children: [
+                                                  Container(
+                                                    child: Text('Saravana Bhavan',
+                                                        style: TextStyle(fontSize: 16)),
                                                   ),
-                                                  child: CircleAvatar(
-                                                    radius: 3,
-                                                    backgroundColor: Colors.green,
+                                                  Container(
+                                                    padding: EdgeInsets.all(3),
+                                                    decoration: BoxDecoration(
+                                                      borderRadius: BorderRadius.circular(4),
+                                                      border: Border.all(),
+                                                    ),
+                                                    child: CircleAvatar(
+                                                      radius: 3,
+                                                      backgroundColor: Colors.green,
+                                                    ),
                                                   ),
-                                                ),
-                                                Expanded(
-                                                  child: Container(
-                                                      child: SizedBox(
-                                                        height:15,
-                                                        // width: 89,
-                                                        child: CustomPaint(
-                                                          painter: PriceTagPaint(),
-                                                          child: Center(
-                                                            child: Text(
-                                                              "Chettinad food",
-                                                              style: TextStyle(
-                                                                fontSize: 9,
+                                                  Expanded(
+                                                    child: Container(
+                                                        child: SizedBox(
+                                                          height: 15,
+                                                          // width: 89,
+                                                          child: CustomPaint(
+                                                            painter: PriceTagPaint(),
+                                                            child: Center(
+                                                              child: Text(
+                                                                "Chettinad food",
+                                                                style: TextStyle(
+                                                                  fontSize: 9,
+                                                                ),
                                                               ),
                                                             ),
                                                           ),
-                                                        ),
-                                                      )
+                                                        )),
+                                                  )
+                                                ],
+                                              )),
+                                          Container(
+                                              child: Row(
+                                                children: [
+                                                  Container(
+                                                    child: Icon(
+                                                      Icons.star,
+                                                      size: 19,
+                                                      color: Colors.yellow,
+                                                    ),
                                                   ),
-                                                )
-                                              ],
-                                            )
-                                        ),
-                                        Container(
-                                            child: Row(
-                                              children: [
-                                                Container(
-                                                  child: Icon(
-                                                    Icons.star,size: 19,color: Colors.yellow,
-                                                  ),
-                                                ),
-                                                Container(child: Text('4.5'),)
-
-                                              ],
-                                            )
-                                        ),
-                                        Divider(color: black,),
-                                        Container(
-                                            child: Text('125/R.S.Puram,Coimbatore,TamilNadu 641002',style: TextStyle(fontSize: 13),)
-                                        ),
-                                      ],
-                                    ),
-                                  )
-                                ]
-                            )
-
-                        ),
-                      )
-                  ), //ListTile
-              childCount: 10,
-            ), //SliverChildBuildDelegate
-          ),
-
-          //SliverList
-        ], //<Widget>[]
-      ) //CustonScrollView
-    );
+                                                  Container(
+                                                    child: Text('4.5'),
+                                                  )
+                                                ],
+                                              )),
+                                          Divider(
+                                            color: black,
+                                          ),
+                                          Container(
+                                              child: Text(
+                                                '125/R.S.Puram,Coimbatore,TamilNadu 641002',
+                                                style: TextStyle(fontSize: 13),
+                                              )),
+                                        ],
+                                      ),
+                                    )
+                                  ])),
+                        ));
+                  }
+              )
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
+
 class PriceTagPaint extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
